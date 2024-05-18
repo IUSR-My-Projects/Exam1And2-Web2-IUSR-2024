a6221430de60e7c89080194234f53ca2d20a914b
MUHMAD OMAR <muhammadaaa727@gmail.com>
2024-05-18 17:51:57 +0300

    Add Files

---
+++ Exam1And2-Web2-IUSR/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Exam1And2_Web2_IUSR.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
